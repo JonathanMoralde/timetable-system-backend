18c9b4e7b12edb7f3452f2c7ae5911fd1ad9304b
Jonathan Moralde <jonathanhernandez.moralde@bicol-u.edu.ph>
2024-04-23 14:28:00 +0800

    project setup

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+
+  // MySQL database credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "bupad";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    session_start();
+
+
+
+
+?>
---
+++ readme.md
@@ -0,0 +1,13 @@
+This is the backend of BUPAD System
+
+Clone this repo to 'C:\xampp\htdocs' using this command:
+
+`git clone <url>`
+
+Then change the folder name from 'bupad_backend' to 'bupad'
+
+Run this command in the project directory after cloning to install dependencies:
+
+`composer install`
+
+Make sure to import the database in phpmyadmin.
